14c70aa45c714b7224161f738b94a8ccae5a3466
Agustin Llado <lladoagustin@gmail.com>
2022-04-07 12:38:13 -0300

    AWS Device Farm configuration

--- Code/src/test/java/android/MobileTestBase.java
+++ Code/src/test/java/android/MobileTestBase.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -42,6 +43,10 @@ public class MobileTestBase {
     //    caps.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/apps/contacts.apk");
         URL url = new URL("http://localhost:4723/wd/hub");
         driver = new AndroidDriver(url, caps);
+    }
+
+    @BeforeMethod
+    public void beforeMethodTest() {
         driver.get(Constants.url);
     }
 
